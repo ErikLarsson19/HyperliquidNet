6b42d8c71590ab4bdcc4e36020dba5c45c25a5ed
ErikLarsson19 <102734798+ErikLarsson19@users.noreply.github.com>
2025-01-15 23:47:53 +0100

    Added more to the Client class, constructors, dispose and httpclient

--- src/HyperliquidClient.cs
+++ src/HyperliquidClient.cs
@@ -6,7 +6,81 @@ using System.Threading.Tasks;
 
 namespace HyperliquidNet.src
 {
-    internal class Class1
+    public class HyperliquidClient : IDisposable
     {
+        private readonly HyperliquidClientOptions _hyperliquidClientOptions;
+        private readonly HttpClient _httpClient;
+        private bool _disposed;
+
+
+        /// <summary>
+        /// Default constructor with default settings
+        /// </summary>
+        public HyperliquidClient()
+            : this(new HyperliquidClientOptions())
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor with specified hyperliquidClientOptions.
+        /// Validates if the options meet the validation criterias.
+        /// Then clones to options so each client gets its independant version of the options.
+        /// </summary>
+        /// <param name="hyperliquidClientOptions">Modified options</param>
+        public HyperliquidClient(HyperliquidClientOptions hyperliquidClientOptions)
+        {
+            if (hyperliquidClientOptions == null)
+                throw new ArgumentNullException(nameof(hyperliquidClientOptions));
+
+            hyperliquidClientOptions.Validate();
+
+            _hyperliquidClientOptions = hyperliquidClientOptions.Clone();
+
+            _httpClient = CreateHttpClient(_hyperliquidClientOptions);
+        }
+
+        /// <summary>
+        /// Creates the http client using the options properties.
+        /// </summary>
+        /// <param name="options">Uses the Timeout and Url set in options</param>
+        /// <returns></returns>
+        private static HttpClient CreateHttpClient(HyperliquidClientOptions options)
+        {
+            var client = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(options.TimeoutSeconds)
+            };
+
+            client.BaseAddress = new Uri(options.BaseUrl);
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        /// <summary>
+        /// Disposes of the client.
+        /// Tells gc not to finalize.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                _httpClient?.Dispose();
+            }
+
+            _disposed = true;
+        }
     }
 }
